ad035d27e8d0f750486cb412f685d27d571d6991
이진영 <112994283+JINyL29@users.noreply.github.com>
2022-09-21 10:26:32 +0900

    2nd_week_1
    aksdflanldksf

---
+++ 2nd_week/prob_1(50p.3.)/prob_1(50p.3.).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{ 
+    printf("Student ID :%d\n", 32165);
+	printf("Name : %s\n", "ÀÌÁø¿µ");
+	printf("Grade : %c\n", 'A');
+
+	return 0;
+
+}
