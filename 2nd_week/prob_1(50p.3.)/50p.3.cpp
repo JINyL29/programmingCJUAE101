974a55f3e72fc4ff22c493ab4d0fd5596d54351a
이진영 <112994283+JINyL29@users.noreply.github.com>
2022-09-21 10:41:40 +0900

    2nd_week_1
    학번과 이름,학점에 대한 코드를 출력하는 방법을 알아보았다.
    정수와 문자열, 문자에 대한 출력데이터를 달리하여 사용해야 한다는 것에 주의하여 코드를 만들었다.

---
+++ 2nd_week/prob_1(50p.3.)/50p.3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("Student ID : %d\n", 32165);
+	printf("Name : %s\n", "jinyoungLee");
+	printf("grade : %c\n", 'A');
+
+	return 0;
+}
